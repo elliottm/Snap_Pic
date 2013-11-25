6c866105fb19d47f554217932f82cc9242c406e9
elliottm <m_elliott@live.com>
2013-11-25 11:58:43 +0000

    start up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BK::Application.config.secret_key_base = 'ceeb08ec92b98589f569ea7f0d454d88fc324ab7f10a7b26082c968c3785f2e67a667eb35b31cfb7d9312660ae89ad698ccfae2f231adb13b99439d3ae9abf6a'
---
+++ features/homepage.feature
@@ -0,0 +1,13 @@
+Feature:
+
+In order to navigate through the website
+As a user
+I want a homepage with login or sign up options
+
+Scenario: Homepage
+
+Given I am on the homepage
+Then I should see "Login"
+Then I should see "Sign Up"
+
+
